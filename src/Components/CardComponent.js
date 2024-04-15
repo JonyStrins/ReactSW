25685026c4095ab73d2a30b5f2d9a52730dc6af4
JonyStrins <slipknot1852@gmail.com>
2024-04-14 23:10:46 -0600

    Commit para merge

--- src/Components/CardComponent.js
+++ src/Components/CardComponent.js
@@ -10,23 +10,27 @@ function CardComponent({ url }) {
         <div>
             {
                 cargando
-                ?
-                <h1>Cargando</h1>
-                :
-                <div className='card' style={{width:'14rem'}} onClick={() => console.log(data.id)}>
-                    <div className='card-header d-flex justify-content-center'>
-                        <h5 className='card-title'>{ data.id } </h5>
+                    ?
+                    <h1>Cargando</h1>
+                    :
+                    <div className='card' style={{ width: '14rem' }} onClick={() => console.log(data.types)}>
+                        <div className='card-header d-flex justify-content-center'>
+                            <h5 className='card-title'>{data.id} </h5>
+                        </div>
+                        <div className='card-body d-flex justify-content-center'>
+                            <img src={data.sprites.front_default} alt='Pokemon' />
+                        </div>
+                        <div className='card-body d-flex justify-content-center'>
+                            <p className='card-text text-capitalize'>{data.forms[0].name}</p>
+                        </div>
+                        <div className='card-footer d-flex justify-content-center'>
+                            {
+                                data.types.map(s => (
+                                    console.log(s)
+                                ))
+                            }
+                        </div>
                     </div>
-                    <div className='card-body d-flex justify-content-center'>
-                        <img src={ data.sprites.front_default } alt='Pokemon'/>
-                    </div>
-                    <div className='card-body d-flex justify-content-center'>
-                        <p className='card-text text-capitalize'>{ data.forms[0].name }</p>
-                    </div>
-                    <div className='card-footer d-flex justify-content-center'>
-                        <p className='card-text text-capitalize'>{ data.forms[0].name }</p>
-                    </div>
-                </div>
             }
         </div>
     );
